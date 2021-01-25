235bff24a2285efe16fc913e428a3a8d9d2ee24a
xiaoze <1416065820@qq.com>
2021-01-25 19:08:31 +0800

    文章搜索完成

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -3,8 +3,18 @@
     <van-cell-group>
       <!-- 搜索历史标题部分 -->
       <van-cell title="搜索历史">
+        <div slot="right-icon" @click="flag=false" v-if="flag">
+          <van-icon name="delete-o"/>
+        </div>
+        <!-- 全部删除按钮 -->
+        <div v-else>
+          <span @click="delAllHistory">全部删除 </span>
+          <span @click="flag=true">完成</span>
+        </div>
+      </van-cell>
+      <van-cell :key="index" v-for="(item, index) in History" :title="item" @click="delThisHistory(item, index)">
         <div slot="right-icon">
-          <van-icon name="delete-o" />
+          <van-icon name="close" v-show="!flag"/>
         </div>
       </van-cell>
     </van-cell-group>
@@ -14,9 +24,30 @@
 <script>
 export default {
   name: 'SearchResult',
-  props: {},
+  props: {
+    History: {
+      type: Array,
+      required: true
+    }
+  },
   data () {
-    return {}
+    return {
+      flag: true // 进行删除按钮的显示与隐藏
+    }
+  },
+  methods: {
+    // 删除单个历史数据
+    delThisHistory (item, index) {
+      if (this.flag) {
+        this.$emit('search', item)
+      } else {
+        this.History.splice(index, 1)
+      }
+    },
+    // 删除全部历史数据
+    delAllHistory () {
+      this.History.splice(0)
+    }
   }
 }
 </script>
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -23,7 +23,12 @@
     @searchResult='onSearch'
     />
     <!-- 搜索历史 -->
-    <SearchHistory v-else></SearchHistory>
+    <SearchHistory
+    v-else
+    :History='historyText'
+    @search='onSearch'
+    >
+    </SearchHistory>
   </div>
 </template>
 
@@ -31,6 +36,8 @@
 import SearchAssocite from './components/search-associate'
 import SearchHistory from './components/search-history'
 import SearchResult from './components/search-result'
+import { setItem, getItem } from '../../utils/storage'
+
 export default {
   name: 'SearchIndex',
   components: {
@@ -41,19 +48,32 @@ export default {
   data () {
     return {
       searchText: '', // 搜索文本
-      flag: false // 设置搜索结果的显示与隐藏
+      flag: false, // 设置搜索结果的显示与隐藏
+      historyText: getItem('SEARCH-HISTORY') || [] // 存储搜索历史
     }
   },
   methods: {
     // 搜索事件
     onSearch (item) {
       this.searchText = item
-      console.log(item)
       this.flag = true
+      // 判断历史数据中有没有重复的数据
+      const index = this.historyText.indexOf(item)
+      console.log(index)
+      if (index !== -1) {
+        this.historyText.splice(index, 1)
+      }
+      this.historyText.unshift(item)
     },
     // 取消事件
     onCancel () {
       this.$router.back()
+    }
+  },
+  watch: {
+    historyText (val) {
+      console.log(val, 'val')
+      setItem('SEARCH-HISTORY', val)
     }
   }
 }
